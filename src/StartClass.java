d0d4d4f113d2513293a543832fb55b17a499d094
rzvnovic <meldinarizvanovic@gmail.com>
2024-05-02 14:59:33 +0200

    La till Startklasskoden

--- src/StartClass.java
+++ src/StartClass.java
@@ -2,18 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
+import oru.inf.InfDB;
+import oru.inf.InfException;
 /**
  *
  * @author meldi
  */
 public class StartClass {
-
+    private static InfDB idb;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try{
         // TODO code application logic here
+        idb = new InfDB("ngo_2024","3306", "dbAdmin2024","dbAdmin2024PW");
+        //new Inloggning(idb).setVisible(true);
+        System.out.println("funkar");
+    }
+        catch (InfException ex){
+            System.out.println(ex.getMessage());
+        }
     }
     
 }
