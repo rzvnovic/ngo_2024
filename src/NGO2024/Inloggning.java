b499f50837ab3067b811b3a2503ae868d77a4070
rzvnovic <meldinarizvanovic@gmail.com>
2024-05-08 11:22:22 +0200

    Fixat i inloggning och handläggarmenyn så att den öppnas om en handläggare lloggar in

--- src/NGO2024/Inloggning.java
+++ src/NGO2024/Inloggning.java
@@ -146,7 +146,7 @@ public class Inloggning extends javax.swing.JFrame {
                 }
                 //om de är projektledare kommer en meny öppnas
                 else if (ärProjektLedare == true){
-                    System.out.println("hej");
+                    new MenyHandläggare(idb, ePost).setVisible(true);
                 }
     
             }
