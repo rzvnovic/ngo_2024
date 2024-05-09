677f21ac59df6389348de0413d6c26c03af749f6
cyruszf <168638238+cyruszf@users.noreply.github.com>
2024-05-09 13:23:41 +0200

    påbörjat validering
    todo koppla till databas

--- src/NGO2024/Validering.java
+++ src/NGO2024/Validering.java
@@ -4,6 +4,8 @@
  */
 package NGO2024;
 
+import oru.inf.InfException;
+
 /*import oru.inf.InfException;
 import oru.inf.InfDB;
 
@@ -22,8 +24,59 @@ public class Validering {
                 }
                 return giltigtFormat;
         }
+    
+
+    /**
+     * Check admin
+     * @param ePost
+     * @return true om admin, false om inte admin
+     */
+    private Boolean checkAdmin (String ePost)
+    {
+        Boolean adminHittad = false; 
         
+        try { 
+            String sqlFrågaAdmin = "select aid from admin where aid in (select aid from anstalld where epost = '" + ePost + "')";
+            String anställningsIDS = idb.fetchSingle(sqlFrågaAdmin);
+            
+        if (anställningsIDS != null)
+        {
+           int anställningsIDInt = Integer.parseInt(anställningsIDS);
+            adminHittad = true;
+        }
         
+        }
+        
+        catch(NumberFormatException | InfException ex){
+            ex.printStackTrace();
+            
+        }
+        return adminHittad; 
+    }
+    
+    private Boolean checkProjektLedare (String ePost)
+    {
+        Boolean projektLedareHittad = false; 
+        
+        try { 
+            String sqlFrågaProjekt = "select projektchef from projekt where projektchef in (select aid from anstalld where epost = '" + ePost + "')";
+            String anställningsIDString = idb.fetchSingle(sqlFrågaProjekt);
+            
+        if (anställningsIDString != null)
+        {
+            //nedanstående rad kan eventuellt tas bort, testa 
+           int anställningsIDInt = Integer.parseInt(anställningsIDString);
+            projektLedareHittad = true;
+        }
+        
+        }
+        
+        catch(NumberFormatException | InfException ex){
+            ex.printStackTrace();
+            
+        }
+        return projektLedareHittad; 
+    }
             
 }
 
